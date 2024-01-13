c36684e3253771f02f0249fe155f000245c8afa4
anurodhacharya <anurodh.acharya10@gmail.com>
2024-01-13 23:42:49 +0545

    finding xpath and processing

--- spamBot.py
+++ spamBot.py
@@ -53,3 +53,50 @@ def submit(inputElement):
         inputElement.click()
     except:
         pass
+
+'''
+This function will retrieve all the forms that are present within a webpage.
+'''
+def retrieve_forms(url):
+    res = session.get(url)
+    soup = BeautifulSoup(res.html.html, "html.parser")
+    return soup.find_all("form")
+
+'''
+This function will retrieve the xpath value of the element that it has been passed with.
+'''
+def find_xpath(tag):
+    items = []
+    present = tag if tag.name else tag.parent
+    while present.parent:
+        siblings = present.parent.find_all(present.name, recursive=False)
+        index = siblings.index(present) + 1 if len(siblings) > 1 else None
+        items.append(f"{present.name}{f'[{index}]' if index else ''}")
+        present = present.parent
+    items.reverse()
+    return '/'.join(items)
+
+'''
+This function is responsible for checking if the value in the element field of the website matches with our required values.
+'''
+def processing(name, id, type, regex, inputElement, isMailSubmit=False, isNameSubmit=False, isPhoneSubmit=False, isSubmit=False):
+    ctr = 0
+    items = regex.split(" ")
+    for word in items:
+        if name == word:
+            ctr+=1
+    for word in items:
+        if id == word:
+            ctr+=1
+    for word in items:
+        if type == word:
+            ctr+=1
+    
+    if ctr >= 1 and isMailSubmit == True:
+        send_email(inputElement)
+    if ctr >= 1 and isNameSubmit == True:
+        send_name(inputElement)
+    if ctr >= 1 and isPhoneSubmit == True:
+        send_contact(inputElement)
+    if isSubmit == True:
+        submit(inputElement)
